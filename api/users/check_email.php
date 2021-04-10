d0e0195fa82a0ed7112e91c318523b7e917cb32f
seahahn <seah.ahn.nt@gmail.com>
2021-04-10 15:02:05 +0900

    User function fixed

--- api/users/check_email.php
+++ api/users/check_email.php
@@ -5,27 +5,37 @@
     $regEmail = '/^[a-zA-Z]{1}[a-zA-Z0-9.\-_]+@[a-z0-9]{1}[a-z0-9\-]+[a-z0-9]{1}\.(([a-z]{1}[a-z.]+[a-z]{1})|([a-z]+))$/';
 
     if($email != ""){
-        $result = mq("SELECT email FROM user WHERE email = '".$email."'");
+        $result = mq("SELECT * FROM user WHERE email = '".$email."'");
         $num = mysqli_num_rows($result);
+        $user = mysqli_fetch_array($result);
+        $inway = $user['inway'];
         if(!preg_match($regEmail, $email)) {
             $ret['msg'] = "올바르지 않은 이메일입니다.";
             $ret['result'] = false;
+            $ret['overlap'] = false;
+        }
+        else if($num != 0 && $inway != 'etc'){ // 이미 SNS를 통해 서비스 이용 중인 경우
+            $ret['msg'] = "";
+            $ret['result'] = true;
+            $ret['overlap'] = true;
         }
         else if($num!=0){
             $ret['msg'] = "이미 가입된 이메일입니다.";
             $ret['result'] = false;
             $ret['overlap'] = true;
-        } else {
+        }
+        else {
             $ret['msg'] = "사용 가능한 이메일입니다.";
             $ret['result'] = true;
+            $ret['overlap'] = false;
         }
-
-        echo json_encode($ret);
 
     } else {
         $ret['msg'] = "이메일을 입력해주세요.";
         $ret['result'] = false;
-
-        echo json_encode($ret);
+        $ret['overlap'] = false;
     }
+
+    echo json_encode($ret);
+
 ?>
