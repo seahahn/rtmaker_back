3de3065c893043b4f4e8041a05eb222426950105
seahahn <seah.ahn.nt@gmail.com>
2021-05-17 23:55:29 +0900

    done function upgraded

--- api/main/done_rt.php
+++ api/main/done_rt.php
@@ -9,14 +9,14 @@
  * 반복하면 완료 여부 변경과 함께 다음 수행 예정일에 같은 내용을 가진 할 일을 생성함
 */
 include_once $_SERVER["DOCUMENT_ROOT"]."/util/db_con.php";
-include_once "fun.php";
+include_once "fun_done.php"; // 루틴 및 할 일 완료 처리를 위한 메소드를 모아둔 파일
 
 $id = $_GET['id']; // 루틴(할 일) 고유 번호
 $done = $_GET['done']; // 완료 구분(0이면 미완료, 1이면 완료)
 $m_date = $_GET['m_date']; // 다음 수행 날짜(루틴이거나 반복하는 할 일인 경우) 또는 당일 날짜
 $done_date = $_GET['done_date']; // 루틴을 수행한 날짜(당일 날짜)
 
-// 루틴(할 일) 처리하기(완료 or 미완료)
+// 루틴 or 할 일 수행 여부 처리하기(완료 or 미완료)
 $done_update = mq("UPDATE rt_todo SET done = '$done' WHERE id='$id'");
 
 // 완료 처리한 루틴(할 일)의 데이터 가져오기
@@ -36,45 +36,14 @@ if($done == 1 && $m_date != ""){
         // 루틴 수행 데이터(과거 내역) 추가하기
         // 루틴 수행 데이터(과거 내역)가 추가되어 있는지 확인 후,
         // 추가되어 있으면 이 데이터의 완료 여부(done 값)을 1로 변경
-        $check = mq("SELECT * FROM rt_done WHERE rt_id = '$result[id]' AND m_date = '$done_date'");
-        $count = mysqli_num_rows($check);
-        if($count == 0) {
-            mq("INSERT rt_done SET
-            title = '$result[title]',
-            m_days = '$result[m_days]',
-            m_date = '$done_date',
-            m_time = '$result[m_time]',
-            rt_id = '$result[id]',
-            user_id = '$result[user_id]',
-            done = '$done'
-            ");
-        } else {
-            mq("UPDATE rt_done SET
-            done = '$done'
-            WHERE rt_id = '$result[id]' AND m_date = '$done_date'
-            ");
-        }
+        done_rt($id, $result['title'], $result['m_days'], $done_date, $result['m_time'], $result['memo'], $result['user_id'], $done);
 
         // 루틴에 포함된 행동 전체 완료 처리
-        mq("UPDATE action SET done = 1 WHERE rt_id = '$id'");
-        done_actions($id, $done_date, 1);
+        done_actions($id, $done_date, $done);
     } else {
         // 완료 처리한 할 일의 다음 수행 날짜 데이터가 있는지 없는지 확인하기
-        $check = mq("SELECT * FROM rt_todo WHERE title = '$result[title]' AND m_date = '$m_date'");
-        $count = mysqli_num_rows($check);
-
         // 할 일과 동일한 제목, 그리고 다음 수행 날짜를 가진 데이터가 없으면 다음에 수행할 할 일 추가함
-        if($count == 0) mq("INSERT rt_todo SET
-        m_type = '$result[m_type]',
-        title = '$result[title]',
-        m_days = '$result[m_days]',
-        alarm = '$result[alarm]',
-        m_date = '$m_date',
-        m_time = '$result[m_time]',
-        on_feed = '$result[on_feed]',
-        memo = '$result[memo]',
-        user_id = '$result[user_id]'
-        ");
+        done_todo($result['title'], $type, $result['m_days'], $m_date, $result['m_time'], $result['alarm'], $result['on_feed'], $result['memo'], $result['user_id']);        
     }
 } else if($done == 0 && $type == "rt") {
     // 오늘(루틴을 수행 가능한) 날짜로 루틴의 수행 예정일 되돌려놓기
@@ -85,16 +54,10 @@ if($done == 1 && $m_date != ""){
 
     // 루틴 수행 데이터(과거 내역)가 추가되어 있는지 확인 후,
     // 추가되어 있으면 이 데이터의 완료 여부(done 값)을 0으로 변경
-    $check = mq("SELECT * FROM rt_done WHERE rt_id = '$result[id]' AND m_date = '$done_date'");
-    $count = mysqli_num_rows($check);
-    if($count != 0) mq("UPDATE rt_done SET
-    done = '$done'
-    WHERE rt_id = '$result[id]' AND m_date = '$done_date'
-    ");
+    done_rt($id, $result['title'], $result['m_days'], $done_date, $result['m_time'], $result['memo'], $result['user_id'], $done);
 
     // 루틴에 포함된 행동을 미완료 상태로 바꾸기
-    mq("UPDATE action SET done = 0 WHERE rt_id = '$id'");
-    done_actions($id, $done_date, 0);
+    done_actions($id, $done_date, $done);
 }
 
 $response;
--- api/main/get_rts.php
+++ api/main/get_rts.php
@@ -31,11 +31,15 @@ if(!$past) {
     // 과거인 경우
 
     // 루틴 수행 데이터(과거 내역)과 함께 할 일 데이터를 가져옴
-    $sql = "SELECT rt_id AS id, m_type, title, m_days, m_date, m_time, alarm, on_feed, memo, user_id, done
-            FROM rt_done WHERE user_id='$user_id' AND m_type = 'rt'
-            UNION
-            SELECT id, m_type, title, m_days, m_date, m_time, alarm, on_feed, memo, user_id, done
-            FROM rt_todo WHERE user_id='$user_id' AND m_type = 'todo'
+    $sql = "SELECT * FROM
+                (
+                SELECT rt_id AS id, m_type, title, m_days, m_date, m_time, alarm, on_feed, memo, user_id, done
+                FROM rt_done WHERE user_id='$user_id' AND m_type = 'rt'
+                UNION
+                SELECT id, m_type, title, m_days, m_date, m_time, alarm, on_feed, memo, user_id, done
+                FROM rt_todo WHERE user_id='$user_id' AND m_type = 'todo'
+                )
+            ORDER BY m_date ASC, m_time ASC
             ";
             // ORDER BY m_date ASC, m_time ASC
             // 
