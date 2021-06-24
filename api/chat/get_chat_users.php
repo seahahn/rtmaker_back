b9de692feca722c3b72f7263f840d9bdf81dfb8e
seahahn <seah.ahn.nt@gmail.com>
2021-06-24 19:38:00 +0900

    getting user data when chat fixed

--- api/chat/get_chat_users.php
+++ api/chat/get_chat_users.php
@@ -3,20 +3,23 @@ include_once $_SERVER["DOCUMENT_ROOT"]."/util/db_con.php";
 
 $room_id = $_GET['room_id'];
 
-$mq = mq("SELECT * FROM chat_user 
-        WHERE room_id='$room_id' AND is_in=1");
+$sql = "SELECT * FROM chat_user WHERE room_id='$room_id' AND is_in=1 ORDER BY created_at ASC";
+$result = mq($sql);
 
-// 채팅방 데이터 보내기
-$result = $mq->fetch_assoc();
-$data = [
-        'id'   => $result['id'],
-        'roomId'   => $result['room_id'],
-        'userId' => $result['user_id'],
-        'isIn' => $result['is_in'],
-        'token'   => $result['token'],
-        'createdAt' => $result['created_at']
-        ];
+// 채팅 참여자 데이터 보내기
+$array = array(); // 앱에 전달할 JSON 데이터를 담을 array.
+while($member = $result->fetch_assoc()) {
+        $data = [
+                'id'   => $member['id'],
+                'roomId'   => $member['room_id'],
+                'userId' => $member['user_id'],
+                'isIn' => $member['is_in'],
+                'token'   => $member['token'],
+                'createdAt' => $member['created_at']
+                ];
+        array_push($array, $data);
+}
 
-echo json_encode($data);
+echo json_encode($array);
 
 ?>
