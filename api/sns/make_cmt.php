eed5a995f4a9c96bde2b266af8da2ce16f091aee
seahahn <seah.ahn.nt@gmail.com>
2021-07-07 22:04:42 +0900

    make_cmt noti function changed

--- api/sns/make_cmt.php
+++ api/sns/make_cmt.php
@@ -28,9 +28,9 @@ if(isset($content)) {
 $response;
 
 if($mq) {
-    $mq_feed = mq("SELECT * FROM sns_newsfeed WHERE id='$feed_id'");
-    $ret_feed = mysqli_fetch_array($mq_feed);
-    $feed_content = $ret_feed['content'];
+    // $mq_feed = mq("SELECT * FROM sns_newsfeed WHERE id='$feed_id'");
+    // $ret_feed = mysqli_fetch_array($mq_feed);
+    // $feed_content = $ret_feed['content'];
     // $images = $ret_feed['images'];
 
     $mq_feed_writer = mq("SELECT token FROM user WHERE id = '$feed_writer_id'");
@@ -40,14 +40,34 @@ if($mq) {
     $is_sub == 1 ? $sub_cmt = true : $sub_cmt = false;
 
     if($mq_feed && $mq_feed_writer) {
-        $response = [
-            'result'   => true,
-            'msg' => "작성되었습니다.",
-            'subCmt' => $sub_cmt,
-            'token' => $feed_writer_token,
-            'content' => $feed_content,
-            'images' => $image
-        ];
+        if($sub_cmt) { // 대댓글인 경우 대댓글이 달린 댓글 작성자와 피드 작성자 둘 모두에게 알림을 보냄
+            $mq_cmt_writer_id = mq("SELECT writer_id FROM feed_comment WHERE main_cmt = '$main_cmt'");
+            $ret_cmt_writer_id = mysqli_fetch_array($mq_cmt_writer_id);
+            $cmt_writer_id = $ret_cmt_writer_id['writer_id'];
+
+            $mq_cmt_writer = mq("SELECT token FROM user WHERE id = '$cmt_writer_id'");
+            $ret_cmt_writer = mysqli_fetch_array($mq_cmt_writer);
+            $cmt_writer_token = $ret_cmt_writer['token'];
+
+            $response = [
+                'result'   => true,
+                'msg' => "작성되었습니다.",
+                'subCmt' => $sub_cmt,
+                'token' => $feed_writer_token,
+                'mainCmtWriterToken' => $cmt_writer_token,
+                'content' => $content,
+                'images' => $image
+            ];
+        } else { // 그냥 댓글이면 피드 작성자에게만 알림 보냄
+            $response = [
+                'result'   => true,
+                'msg' => "작성되었습니다.",
+                'subCmt' => $sub_cmt,
+                'token' => $feed_writer_token,
+                'content' => $content,
+                'images' => $image
+            ];
+        }
     }
 } else {
     $response = [
