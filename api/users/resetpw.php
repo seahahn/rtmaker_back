168333ee8cd9dec8e98697ff92cfff26c4be9ee4
seahahn <seah.ahn.nt@gmail.com>
2021-08-01 19:17:07 +0900

    resetpw test

--- api/users/resetpw.php
+++ api/users/resetpw.php
@@ -64,10 +64,11 @@ try {
     $mail -> Host = "smtp.gmail.com";                // email 보낼때 사용할 서버를 지정
     $mail -> SMTPAuth = true;                        // SMTP 인증을 사용함
     $mail -> Username = "rtmaker.noreply@gmail.com";    // 메일 계정
-    $mail -> Password = "Teamnova123!";                // 메일 비밀번호
+    $mail -> Password = "TeamNova123!";                // 메일 비밀번호
     $mail -> SMTPSecure = "ssl";                    // SSL을 사용함
     $mail -> Port = 465;                            // email 보낼때 사용할 포트를 지정
     $mail -> CharSet = "utf-8";                        // 문자셋 인코딩
+    $mail -> Encoding = "base64";
     echo "메일 계정 설정";
 
     // 보내는 메일
