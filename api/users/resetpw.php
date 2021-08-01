b260c58244feb58e881cbd2fc77730766b9f0916
seahahn <seah.ahn.nt@gmail.com>
2021-08-01 19:05:35 +0900

    resetpw test

--- api/users/resetpw.php
+++ api/users/resetpw.php
@@ -60,6 +60,7 @@ try {
     // 메일 서버세팅
     $mail -> SMTPDebug = false;    // 디버깅 설정
     $mail -> isSMTP();        // SMTP 사용 설정
+    echo "디버깅, SMTP 사용 설정";
 
     $mail -> Host = "smtp.gmail.com";                // email 보낼때 사용할 서버를 지정
     $mail -> SMTPAuth = true;                        // SMTP 인증을 사용함
@@ -68,12 +69,14 @@ try {
     $mail -> SMTPSecure = "ssl";                    // SSL을 사용함
     $mail -> Port = 465;                            // email 보낼때 사용할 포트를 지정
     $mail -> CharSet = "utf-8";                        // 문자셋 인코딩
+    echo "메일 계정 설정";
 
     // 보내는 메일
     $mail -> setFrom("rtmaker.noreply@gmail.com", "no-reply");
 
     // 받는 메일    
     $mail -> addAddress("$email", "$nick");
+    echo "보내는 메일, 받는 메일 설정";
 
     // 메일 내용
     $mail -> isHTML(true);                                               // HTML 태그 사용 여부
@@ -184,6 +187,7 @@ try {
 
     </td></tr></tbody></table>
     ';    // 메일 내용
+    echo "메일 내용 설정";
 
     // Gmail로 메일을 발송하기 위해서는 CA인증이 필요하다.
     // CA 인증을 받지 못한 경우에는 아래 설정하여 인증체크를 해지하여야 한다.
@@ -194,9 +198,11 @@ try {
             , "allow_self_signed" => true
         )
     );
+    echo "CA 인증 설정";
 
     // 메일 전송
     $mail -> send();
+    echo "메일 전송";
 
     // DB에 사용자 비번과 해쉬값 정보 저장
     $mq = mq("UPDATE user set
@@ -204,6 +210,7 @@ try {
                 WHERE
                 email = '$email'
                 ");
+    echo "DB 업데이트";
 
     $response = [
         'result'   => true,
