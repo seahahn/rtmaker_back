71c9df7a91e52500dd932dd0677b5d0d4e8a52bb
seahahn <seah.ahn.nt@gmail.com>
2021-08-24 18:09:26 +0900

    delete rt, action function fixed

--- api/main/delete_action.php
+++ api/main/delete_action.php
@@ -5,6 +5,9 @@ $id = $_POST['id'];
 
 $mq = mq("DELETE FROM action WHERE id='$id'");
 
+$today = date("Y-m-d", time()); // 오늘 날짜 구하기
+mq("DELETE FROM action_done WHERE action_id='$id' AND m_date='$today'"); // 오늘 날짜에 행동의 수행 데이터 남아있는 경우 삭제시키기
+
 $response;
 
 if($mq) {
