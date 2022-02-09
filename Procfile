829c876c4d917029015c021d3d828b065cc78588
seahahn <seah.ahn.nt@gmail.com>
2022-02-09 22:50:47 +0900

    Procfile added & db_con content concealed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: heroku-php-apache2 | cd ./chatsocket/rtmaker_socket | ./gradlew run
--- util/db_con.php
+++ util/db_con.php
@@ -1,10 +1,8 @@
 <?php
-    $db_id="root"; // DB 계정명
-    $db_pw="0121"; // DB 계정 비밀번호(aws)
-    // $db_pw=""; // DB 계정 비밀번호(데스크탑)
-    // $db_pw="teamnova123"; // DB 계정 비밀번호(랩탑)
-    $db_name="rtmaker"; // 연결할 데이터베이스명
-    $db_domain="127.0.0.1"; // 연결할 도메인
+    $db_id=$_SERVER['DB_USERID']; // DB 계정명
+    $db_pw=$_SERVER['DB_PW']; // DB 계정 비밀번호(aws)
+    $db_name=$_SERVER['DB_NAME']; // 연결할 데이터베이스명
+    $db_domain=$_SERVER['DB_HOST']; // 연결할 도메인
     $db=mysqli_connect($db_domain,$db_id,$db_pw,$db_name);
 
     // SQL 쿼리문 간단하게 쓰기 위한 함수 mq 선언
